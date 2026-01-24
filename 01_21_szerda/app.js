9f12eb389b7387bf89cec66c6a00d22590455734
Pannanna <korosanna95@gmail.com>
2026-01-24 18:50:46 +0100

    class book

--- 01_21_szerda/app.js
+++ 01_21_szerda/app.js
@@ -1,1 +1,10 @@
 console.log("Házi feladat");
+var Book = /** @class */ (function () {
+    function Book(id, title, author, price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    return Book;
+}());
--- 01_21_szerda/app.ts
+++ 01_21_szerda/app.ts
@@ -1,3 +1,15 @@
 console.log("Házi feladat");
 
+class Book {
+  id: string;
+  title: string;
+  author: string;
+  price: number;
 
+  constructor(id: string, title: string, author: string, price: number) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.price = price;
+  }
+}
