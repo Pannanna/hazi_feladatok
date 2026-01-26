4cdcdbd7d97a106964ed0318cef31ec6cf38f4eb
Pannanna <korosanna95@gmail.com>
2026-01-26 13:09:27 +0100

    hazi

--- 01_21_szerda/app.js
+++ 01_21_szerda/app.js
@@ -8,3 +8,39 @@ var Book = /** @class */ (function () {
     }
     return Book;
 }());
+var Library = /** @class */ (function () {
+    function Library() {
+        this.books = [];
+    }
+    Library.prototype.addBook = function (book) {
+        this.books.push(book);
+    };
+    Library.prototype.removeBook = function (id) {
+        this.books = this.books.filter(function (book) { return book.id !== id; });
+    };
+    Library.prototype.findBookById = function (id) {
+        return this.books.find(function (book) { return book.id === id; });
+    };
+    Library.prototype.listAllBooks = function () {
+        return this.books;
+    };
+    return Library;
+}());
+var User = /** @class */ (function () {
+    function User(userId, name, email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+    User.prototype.borrowBook = function (library, bookId) {
+        var book = library.findBookById(bookId);
+        if (book) {
+            library.removeBook(bookId);
+            console.log("".concat(this.name, " kik\u00F6lcs\u00F6n\u00F6zte: \"").concat(book.title, "\""));
+        }
+        else {
+            console.log("Nincs ilyen könyv a könyvtárban!");
+        }
+    };
+    return User;
+}());
--- 01_21_szerda/app.ts
+++ 01_21_szerda/app.ts
@@ -1,5 +1,6 @@
 console.log("Házi feladat");
 
+
 class Book {
   id: string;
   title: string;
@@ -13,3 +14,50 @@ class Book {
     this.price = price;
   }
 }
+
+
+class Library {
+  books: Book[] = []; 
+
+  addBook(book: Book): void {
+    this.books.push(book);
+  }
+
+  removeBook(id: string): void {
+    this.books = this.books.filter((book) => book.id !== id);
+  }
+
+  findBookById(id: string): Book | undefined {
+    return this.books.find((book) => book.id === id);
+  }
+
+  listAllBooks(): Book[] {
+    return this.books;
+  }
+}
+
+
+class User {
+  userId: string;
+  name: string;
+  email: string;
+
+  constructor(userId: string, name: string, email: string) {
+    this.userId = userId;
+    this.name = name;
+    this.email = email;
+  }
+
+  borrowBook(library: Library, bookId: string): void {
+    const book = library.findBookById(bookId);
+
+    if (book) {
+      library.removeBook(bookId);
+      console.log(`${this.name} kikölcsönözte: "${book.title}"`);
+    } else {
+      console.log("Nincs ilyen könyv a könyvtárban!");
+    }
+  }
+}
+
+
